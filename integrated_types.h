843c0f5e4517c40c08209e36f62f0633a0b6832d
bonamana <121037947+ilsailo@users.noreply.github.com>
2026-02-17 21:06:21 +0900

    Update integrated_types.h

--- integrated_types.h
+++ integrated_types.h
@@ -1,18 +1,30 @@
-#ifndef INTEGRATED_TYPES_H
-#define INTEGRATED_TYPES_H
+#ifndef SHARED_H
+#define SHARED_H
 
-#include <time.h>
-#include <stdbool.h>
-bo
-// [Shared State] 모든 모듈이 참조하는 최신 시스템 상태
+#include <mqueue.h>
+#include <fcntl.h>
+
+// 4개의 독립적인 POSIX 메시지 큐 이름
+#define MQ_TH      "/mq_th"
+#define MQ_VITAL   "/mq_vital"
+#define MQ_DB      "/mq_db"
+#define MQ_SEND    "/mq_send"
+
+// 1, 2번 모듈이 보내는 원본 데이터 구조
 typedef struct {
-    double temperature;      // 모듈 1(센서) 데이터
-    double humidity;         // 모듈 1(센서) 데이터
-    double heartRate;        // 모듈 2(워치) 데이터
-    double skin_temperature; // 모듈 2(워치) 데이터
-    
-    struct timespec last_sync; // 마지막 동기화 시간
-    bool emergency_detected;   // 모듈 3(판단) 결과
-} SystemState;
+    int type;          // 1: 온습도, 2: 워치
+    float val1;        // 온도/심박수
+    float val2;        // 습도/체온
+    long timestamp;
+// 물리적 설계에 맞춰서 향후에 수정 해야 할 것임.
+} RawData;
+
+// 3번(룰베이스)이 4번(DB)으로 넘기는 통합 데이터 구조
+typedef struct {
+    RawData raw;       // 원본 데이터 포함
+    int status_code;   // 판단 결과
+    char msg[32];      // 상태 메시지
+// 물리적 설계를 보면, 원본 데이터들 + 이상상태 + 알림 + 센서데이터 패키징으로 만들어서 보내야할 것 같음
+} DBData;
 
 #endif
